4595bbae4c966c45d8caa81701cbfab80e888d5a
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-12-01 11:16:52 +0100

    scss generale, font,navbar ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DoneComponent } from './components/done/done.component';
 import { ToDoComponent } from './components/to-do/to-do.component';
+import { Route, RouterModule } from '@angular/router';
 
+const routes: Route[] = [
+  {
+    path: '',
+    component: ToDoComponent,
+  },
+  {
+    path: 'done',
+    component: DoneComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    DoneComponent,
-    ToDoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, DoneComponent, ToDoComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.scss']
+  styleUrls: ['./to-do.component.scss'],
 })
 export class ToDoComponent implements OnInit {
+  constructor(private serv: TodoService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
